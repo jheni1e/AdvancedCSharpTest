5e50b72960d8f493d22b41b5fdc915098632e3e0
Jhenifer Halma <highhalma@gmail.com>
2025-09-10 15:14:24 -0300

    points.sql added

---
+++ points.sql
@@ -0,0 +1,12 @@
+use RideClub
+
+select * from Users
+select * from Rides
+select * from Points
+
+INSERT INTO Points ([Name]) VALUES ('Asteristico Cafe')
+INSERT INTO Points ([Name]) VALUES ('Lucca Cafes Especiais')
+INSERT INTO Points ([Name]) VALUES ('Emily Doces')
+INSERT INTO Points ([Name]) VALUES ('Biel Lanches')
+INSERT INTO Points ([Name]) VALUES ('Bar Mykola')
+INSERT INTO Points ([Name]) VALUES ('The Coffee Nex')
