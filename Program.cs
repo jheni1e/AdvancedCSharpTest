e0713383e67774302d074df326be3f15f686ca95
Jhenie <highhalma@gmail.com>
2025-09-10 14:27:56 -0300

    usecases added as transients

--- Program.cs
+++ Program.cs
@@ -2,14 +2,34 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using RideClub.Endpoints;
 using RideClub.Models;
+using RideClub.Services.JWT;
+using RideClub.Services.Profile;
+using RideClub.Services.Rides;
+using RideClub.UseCases.CreateProfile;
+using RideClub.UseCases.CreateRide;
+using RideClub.UseCases.EditRide;
+using RideClub.UseCases.GetRide;
+using RideClub.UseCases.Login;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<RideClubDbContext>(options => {
+builder.Services.AddDbContext<RideClubDbContext>(options =>
+{
     var sqlConn = "Data Source=localhost; Initial Catalog=RideClub; Trust Server Certificate=true; Integrated Security=true";
     options.UseSqlServer(sqlConn);
 });
+
+builder.Services.AddTransient<IProfileService, EFProfileService>();
+builder.Services.AddTransient<IRideService, EFRideService>();
+builder.Services.AddSingleton<IJWTService, JWTService>();
+
+builder.Services.AddTransient<CreateProfileUseCase>();
+builder.Services.AddTransient<CreateRideUseCase>();
+builder.Services.AddTransient<EditRideUseCase>();
+builder.Services.AddTransient<GetRideUseCase>();
+builder.Services.AddTransient<LoginUseCase>();
 
 var jwtSecret = "adbfoahfodsahoifdsjoifjdssoidjfdsfojsfa";
 var keyBytes = Encoding.UTF8.GetBytes(jwtSecret);
@@ -45,5 +65,9 @@ app.UseAuthorization();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.ConfigureAuthEndpoints();
+app.ConfigureRideEndpoints();
+app.ConfigureUserEndpoints();
 
 app.Run();
